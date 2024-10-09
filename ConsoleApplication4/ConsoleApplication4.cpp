359fbf58eb9ae2558da47236bb99f9670cfafd4f
User <User@LAPTOP-3MJSQ1I4>
2024-10-09 23:42:04 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication4/ConsoleApplication4.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <string>
+using namespace std;
+
+void Kalk(string n, int a)
+{
+    cout << n << " " << a << endl;
+}
+
+int Summa(int a, int b)
+{
+    return a + b;
+}
+
+int Razn(int a, int b)
+{
+    return a - b;
+}
+
+int Umn(int a, int b)
+{
+    return a * b;
+}
+
+int del(int e, int g)
+{
+    if (g != 0) {
+        return e / g;
+    }
+    else {
+        cout << "деление на ноль!" << endl;
+        return 0; 
+    }
+} 
+
+int main() {
+    Kalk("Summa = ", Summa(25, -44));
+    Kalk("Razn = ", Razn(23, -44));
+    Kalk("Umn = ", Umn(23, -44));
+    Kalk("Del = ", del(23, -44)); 
+}
